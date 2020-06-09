a7d37b20c085e42ea2d7f685d311630078a3ca61
John Crannell-Ash <me@jcrash.com>
2020-06-09 19:40:38 -0400

    I give up, dont fight the river. I will use the IOC to manage the GPIO

--- STM32_Nucleo/InitialBringUp/hardwareAbstractionLayer/hw_UART.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "hardwareDefinitions.h"
-#include "stm32l0xx_hal.h"
-void Uart_init();
-
-void Uart_activate(hw_uart_id_t uartID, USART_TypeDef * uartNumber);
-
-int uprintf(hw_uart_id_t uartID, const char* format, ...);
